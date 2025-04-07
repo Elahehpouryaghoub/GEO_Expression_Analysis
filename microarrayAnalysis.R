7e8ecad3ffe2f8e1f685f18ade3887b12a6ed3a6
Ella <elaheh.pouryaghoub@gmail.com>
2025-04-07 13:45:57 +0200

    Update microarrayAnalysis.R

--- microarrayAnalysis.R
+++ microarrayAnalysis.R
@@ -21,10 +21,12 @@ platform = 'GPL96'    #  microarray platform used (type of chip)
 #### Download the Data
 gset = getGEO( series , GSEMatrix= TRUE , AnnotGPL= TRUE , destdir= 'Data/')
 length(gset)
+
 - Downloads the gene expression data from GEO.
-- `GSEMatrix=TRUE` gets it in matrix format (easier to work with).
-- `AnnotGPL=TRUE` adds gene annotation (gene symbols, etc.).
-- `destdir='Data/'` saves the data locally in a folder called `Data/`.
+- GSEMatrix=TRUE` gets it in matrix format (easier to work with).
+- AnnotGPL=TRUE` adds gene annotation (gene symbols, etc.).
+- destdir='Data/'` saves the data locally in a folder called `Data/`.
+
 #### Select the Correct Platform (if multiple)
 if (length(gset) > 1) idx = grep(platform, attr(gset, "names")) else idx = 1
 gset = gset[[idx]]
